1450d2f63500a83d0c8bf5f142d481a527e86f3b
MartiHavli <marti.havli@centrum.cz>
2026-06-26 12:22:03 +0200

    Add configuration settings for travel planning

---
+++ config.py
@@ -0,0 +1,33 @@
+# config.py
+
+CONFIG = {
+   "departure_city": "Brno",
+
+   "date_from": "2026-07-25",
+   "date_to": "2026-08-02",
+
+   "people": {
+       "adults": 3,
+       "children": 1,
+       "child_ages": [17]
+   },
+
+   "board": "ALL_INCLUSIVE",
+
+   "min_hotel_stars": 4,
+
+   "max_price_per_person": 11000,
+
+   "destinations": [
+       "Egypt",
+       "Marsa Matrouh",
+       "Greece",
+       "Turkey",
+       "Mallorca",
+       "Canary Islands"
+   ],
+
+   "check_interval_minutes": 30,
+
+   "email_to": "marti.havli@centrum.cz"
+}
